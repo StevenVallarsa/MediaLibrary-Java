09b81a5ae6bc039845d17b6b75dd323a6c905825
StevenVallarsa <orionsjv@gmail.com>
2022-01-10 18:13:09 -0500

    first commit

---
+++ src/main/java/com/sv/medialibrary2022/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.sv.medialibrary2022;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-01-10
+ * purpose: Console app to keep track of media (books, tapes, CDs, DVDs, etc...)
+ *          using CRUD MVC pattern
+ */
+public class App {
+
+}
